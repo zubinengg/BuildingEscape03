695a77406d6defbf374eed7349a148346bf8333b
DESKTOP-KK8513T\Ravi <zubin.engg@gmail.com>
2018-04-25 19:59:51 +0530

    Unreal Creatng first CPP class

---
+++ Source/BlockBreaker/PositionReporter.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PositionReporter.h"
+
+
+// Sets default values for this component's properties
+UPositionReporter::UPositionReporter()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+// code will run once
+// thats it :-)
+void UPositionReporter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+
+	
+}
+
+
+// Called every frame
+void UPositionReporter::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
