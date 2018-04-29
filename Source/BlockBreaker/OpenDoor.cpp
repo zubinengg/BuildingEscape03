221b5c26c07ade618ddaa5640129c3a91f70f379
DESKTOP-KK8513T\Ravi <zubin.engg@gmail.com>
2018-04-29 19:26:25 +0530

    Door Open Done

--- Source/BlockBreaker/OpenDoor.cpp
+++ Source/BlockBreaker/OpenDoor.cpp
@@ -1,6 +1,7 @@
 // No CopyRight
 
 #include "OpenDoor.h"
+#include "GameFramework/Actor.h"
 
 
 // Sets default values for this component's properties
@@ -9,7 +10,6 @@ UOpenDoor::UOpenDoor()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
 	// ...
 }
 
@@ -20,6 +20,20 @@ void UOpenDoor::BeginPlay()
 	Super::BeginPlay();
 
 	// ...
+	// Finding the Actor
+	AActor* Owner = GetOwner();
+
+	// Creating a rotator
+
+	//Set Door Rotation
+	FRotator NewRotation = FRotator(0.0f, -60.0f, 0.0f);
+	Owner->SetActorRotation(NewRotation);
+
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectPosition = GetOwner()->GetTransform().GetLocation().ToString();
+
+	UE_LOG(LogTemp, Warning, TEXT("Position Report Reporting for duty on %s is at %s!!!!!!!"), *ObjectName, *ObjectPosition);
+
 	
 }
 
